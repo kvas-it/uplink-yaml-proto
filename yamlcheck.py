553c31948c6fc03ae15f3e27d34173946bb0f2c8
Vasily Kuznetsov <kvas.it@gmail.com>
2016-08-23 16:49:31 +0200

    Improve yamlcheck

--- yamlcheck.py
+++ yamlcheck.py
@@ -1,8 +1,33 @@
 #!/usr/bin/env python
 
-import yaml
+from __future__ import print_function
+
+import argparse
+import code
+import pprint
 import sys
+import yaml
 
 
-data = yaml.load(sys.stdin)
-print yaml.dump(data, default_flow_style=False)
+parser = argparse.ArgumentParser(description='Load and dump a yaml file')
+parser.add_argument('input', type=argparse.FileType('r'), nargs='?',
+                    default='-', help='Input file')
+parser.add_argument('-i', '--interactive', action='store_true',
+                    help='Start interactive session after loading the data')
+parser.add_argument('-p', '--print', action='store_true',
+                    help='Dump the loaded data structure as Python')
+parser.add_argument('-y', '--yaml', action='store_true',
+                    help='Dump the loaded data as YAML')
+args = parser.parse_args()
+
+try:
+    data = yaml.load(args.input)
+except yaml.error.YAMLError as err:
+    sys.exit(err)
+
+if getattr(args, 'print'):
+    pprint.pprint(data)
+if args.yaml:
+    print(yaml.dump(data, default_flow_style=False))
+if args.interactive:
+    code.interact(local={'data': data})
